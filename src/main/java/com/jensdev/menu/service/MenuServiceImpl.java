a86a7de43ed4d5a7857b2e973951238661215b4c
Jens Liu <jensliu39@gmail.com>
2023-06-27 07:32:01 +0800

    added error message and rollback

--- src/main/java/com/jensdev/menu/service/MenuServiceImpl.java
+++ src/main/java/com/jensdev/menu/service/MenuServiceImpl.java
@@ -9,14 +9,18 @@ import com.jensdev.menu.repository.MenuCategoryRepository;
 import com.jensdev.menu.repository.MenuItemFlavourRepository;
 import com.jensdev.menu.repository.MenuItemSizeRepository;
 import com.jensdev.menu.repository.MenuItemRepository;
-import com.jensdev.menu.service.MenuService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Component;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Component
 @RequiredArgsConstructor
+@Log4j2
 public class MenuServiceImpl implements MenuService {
     private final MenuItemRepository menuRepository;
     public final MenuCategoryRepository menuCategoryRepository;
@@ -40,21 +44,54 @@ public class MenuServiceImpl implements MenuService {
 
     @Override
     public MenuItem updateMenuItem(MenuItem menuItem) {
+
         MenuItem old = menuRepository.findById(menuItem.getId()).orElseThrow(() -> new BusinessException("Cannot update non-existent item"));
 
-        List<Long> flavourIds = old.getFlavours().stream().map(MenuItemFlavour::getId).toList();
-        List<Long> sizeIds = old.getSizes().stream().map(MenuItemSize::getId).toList();
-        menuItemFlavourRepository.deleteAllById(flavourIds);
-        menuItemSizeRepository.deleteAllById(sizeIds);
+        System.out.println("old flavours: " + old.getFlavours());
+        System.out.println("old sizes: " + old.getSizes());
+        System.out.println("new flavours: " + menuItem.getFlavours());
+        System.out.println("new sizes: " + menuItem.getSizes());
 
-        return menuRepository.save(menuItem);
+        Set<MenuItemFlavour> deletingFlavours = new HashSet<>(old.getFlavours());
+        Set<MenuItemSize> deletingSizes = new HashSet<>(old.getSizes());
+
+        menuItem.getFlavours().forEach(deletingFlavours::remove);
+        menuItem.getSizes().forEach(deletingSizes::remove);
+
+        MenuItem item = menuRepository.save(menuItem);
+
+        try {
+            log.info("deleting flavours: " + deletingFlavours);
+            log.info("deleting sizes: " + deletingSizes);
+            // deleteAllByIds does not report if it fails
+            menuItemFlavourRepository.deleteAllInBatch(deletingFlavours);
+            menuItemSizeRepository.deleteAllInBatch(deletingSizes);
+        } catch (DataIntegrityViolationException e) {
+            log.info("Some flavour/size might not be removed from database because it already has a reference to it");
+            log.info("rolling back changes");
+            deletingFlavours.addAll(item.getFlavours());
+            deletingSizes.addAll(item.getSizes());
+            item.setFlavours(deletingFlavours.stream().toList());
+            item.setSizes(deletingSizes.stream().toList());
+            System.out.println(item.getFlavours());
+            System.out.println(item.getSizes());
+            menuRepository.save(item);
+            throw new BusinessException("Cannot delete flavour/size because it is used in other places");
+        }
+        return item;
     }
 
     @Override
     public void deleteItem(Long id) {
-        MenuItem item = menuRepository.findById(id).orElseThrow(() -> new BusinessException("Cannot delete non-existent item"));
-        item.setIsDeleted(true);
-        menuRepository.save(item);
+        try {
+            menuRepository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            log.info("Cannot delete item because it is used in other places");
+            throw new BusinessException("Cannot delete item because it is used in other places");
+//            MenuItem item = menuRepository.findById(id).orElseThrow(() -> new BusinessException("Cannot delete non-existent item"));
+//            item.setIsDeleted(true);
+//            menuRepository.save(item);
+        }
     }
 
     @Override
